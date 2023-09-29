4fa6e2faa365e5ae5bdbe707eae89e383db28559
ehtisham mohd <125719040+Ehtisham9198@users.noreply.github.com>
2023-09-29 23:06:54 +0530

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mohd Ehtisham",
+  title: "Web Developer",
+  email: "mohdehtisham@example.com",
+  gitHub: "Ehtisham9198",
+  instagram: "https://instagram.com/ehtisham_119?igshid=OGQ5ZDc2ODk2ZA==",
+  linkedIn: "https://www.linkedin.com/in/ehtishaam",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "ehtisham",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
